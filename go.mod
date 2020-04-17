47155b1f73fe68abab14814bf3d917f07f0fcb13
TRedzepagic <tarik.redzepagic.2012@gmail.com>
2020-04-17 11:42:47 +0200

    Added timer reading capability

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/TRedzepagic/udpclient
 
 go 1.14
+
+require github.com/TRedzepagic/compositelogger v0.0.0-20200326090004-c1d15344ef06
--- main.go
+++ main.go
@@ -5,25 +5,55 @@ import (
 	"fmt"
 	"net"
 	"os"
+
+	"github.com/TRedzepagic/compositelogger/logs"
 )
 
+// Read autoreads UDP packets
+func Read(connection *net.UDPConn, ReplyLog *logs.CompositeLog, TimeLog *logs.CompositeLog) {
+	for {
+		buffer := make([]byte, 1024)
+		n, _, err := connection.ReadFromUDP(buffer)
+		if err != nil {
+			ReplyLog.Error(err)
+			return
+		}
+		if string(buffer[0:n]) == "Timer Tick! : 5 seconds have elapsed" {
+			TimeLog.Info(string(buffer[0:n]))
+		} else {
+			ReplyLog.Info(string(buffer[0:n]))
+			return
+		}
+	}
+}
+
 func main() {
+	fileLog := logs.NewFileLogger("filelog")
+	defer fileLog.Close()
+	stdoutLog := logs.NewStdLogger()
+	defer stdoutLog.Close()
+
+	wantDebug := false
+
+	ErrInfoLog := logs.NewCustomLogger(wantDebug, fileLog, stdoutLog)
+	ReplyLog := logs.NewCustomLogger(wantDebug, stdoutLog)
+	TimeLog := logs.NewCustomLogger(wantDebug, fileLog)
+
 	//Expecting valid address host:port format
 	arguments := os.Args
 	if len(arguments) == 1 {
-		fmt.Println("No port number specified... Exiting.....")
+		ErrInfoLog.Info("No address specified... Exiting.....")
 		return
 	}
-
-	PORT := arguments[1]
-	str, err := net.ResolveUDPAddr("udp4", PORT)
+	addr := arguments[1]
+	str, err := net.ResolveUDPAddr("udp4", addr) //server address
 	connection, err := net.DialUDP("udp4", nil, str)
 	if err != nil {
-		fmt.Println(err)
+		ErrInfoLog.Error(err)
 		return
 	}
 
-	fmt.Printf("Connected to : %s\n", connection.RemoteAddr().String())
+	fmt.Printf("Dialed address : %s\n", connection.RemoteAddr().String())
 	defer connection.Close()
 
 	for {
@@ -34,17 +64,10 @@ func main() {
 		_, err = connection.Write(data)
 
 		if err != nil {
-			fmt.Println(err)
+			ErrInfoLog.Error(err)
 			return
 		}
+		go Read(connection, ReplyLog, TimeLog)
 
-		buffer := make([]byte, 1024)
-		n, _, err := connection.ReadFromUDP(buffer)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-
-		fmt.Printf("Server replies : %s\n", string(buffer[0:n]))
 	}
 }
